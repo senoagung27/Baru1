3c59fc89037d41a551870b61e4cb1ab641386110
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-07-30 11:44:28 +0700

    Update

--- Domain/Value.cs
+++ Domain/Value.cs
@@ -4,9 +4,7 @@ namespace Domain
 {
     public class Value
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
         public string Name { get; set; }
-        
-        
     }
 }
