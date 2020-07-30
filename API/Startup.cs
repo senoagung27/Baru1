499a3a30f0f2a1df63235cc1f7baa389200696ad
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-07-30 14:11:34 +0700

    Update Fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
