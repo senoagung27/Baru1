31022ffd7860281ebe341f6b35951190c97e7037
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-08-04 11:40:41 +0700

    Update

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 
@@ -22,7 +21,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <ul>
             {this.state.values.map((values:any) =>(
               <li key={values.id}>{values.name}</li>
