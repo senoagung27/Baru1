8bf3ed22f6035279bc7a3823c94476afe238e890
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-08-04 13:03:47 +0700

    Add Libray Semantic

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,36 +1,37 @@
-import React, {Component} from 'react';
-import './App.css';
-import axios from 'axios'
+import React, { Component } from "react";
+import "./App.css";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
 
 class App extends Component {
-  state ={
-    values : []
-  }
+  state = {
+    values: [],
+  };
 
-  componentDidMount(){
-    axios.get('http://localhost:5000/api/values')
-    .then((response) =>{
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
       console.log(response);
       this.setState({
-        values: response.data
-      })
-    })  
+        values: response.data,
+      });
+    });
   }
-  
-  render(){
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <ul>
-            {this.state.values.map((values:any) =>(
-              <li key={values.id}>{values.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as="h2">
+          <Icon name="user" />
+          <Header.Content>Baru</Header.Content>
+        </Header>
+          <List>
+          {this.state.values.map((values: any) => (
+          <List.Item  key={values.id}>{values.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
- 
 }
 
 export default App;
